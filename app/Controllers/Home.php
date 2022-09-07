eaa0f3a101b9fd0a49de839b9001fdf28640b506
dx14 <duyx4541@gmail.com>
2022-09-07 14:56:10 +0700

    #minor update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,22 @@
 
 namespace App\Controllers;
 
+use App\Models\MenuModel;
+
 class Home extends BaseController
 {
+    //Api url
+    private string $url;
+    
     public function index()
     {
-        return view('home/index');
+        $menu_m = new MenuModel();
+        $this->url = 'http://localhost:1993/v1/menu';
+        $menu = $menu_m->get_menu($this->url);
+        if ($menu) {
+            $data['menu'] = $menu;
+        }
+
+        return view('home/index', $data);
     }
 }
